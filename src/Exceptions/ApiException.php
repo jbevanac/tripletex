22b08a194c1234fe062d9ec957bea0d99361462f
jbevanac <62661464+jbevanac@users.noreply.github.com>
2025-05-31 14:08:38 +0200

    APIException

---
+++ src/Exceptions/ApiException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tripletex\Exceptions;
+
+class ApiException extends \Exception
+{
+
+}
--- src/Resources/Concerns/CanCreateResource.php
+++ src/Resources/Concerns/CanCreateResource.php
@@ -2,12 +2,11 @@
 
 namespace Tripletex\Resources\Concerns;
 
-namespace Tripletex\Resources\Concerns;
-
 use JustSteveKing\Tools\Http\Enums\Method;
 use Tripletex\Contracts\ModelInterface;
 use Tripletex\Contracts\ResourceInterface;
 use Tripletex\DTO\ErrorResponse;
+use Tripletex\Exceptions\ApiException;
 use Tripletex\Exceptions\FailedToCreateResourceException;
 
 /**
@@ -18,12 +17,11 @@ trait CanCreateResource
     /**
      * Create a resource via POST request.
      *
-     * @param object $dto
-     * @param string $url
-     * @param callable|null $factory A factory to create the DTO from API response data
-     * @return object
+     * @param ModelInterface $dto
      *
+     * @return ModelInterface
      * @throws FailedToCreateResourceException
+     * @throws ApiException
      */
     public function create(ModelInterface $dto): ModelInterface
     {
@@ -48,11 +46,19 @@ trait CanCreateResource
             );
         }
 
-        $responseData = json_decode(
-            json: $response->getBody()->getContents(),
-            associative: true,
-            flags: JSON_THROW_ON_ERROR,
-        );
+        try {
+            $responseData = json_decode(
+                json: $response->getBody()->getContents(),
+                associative: true,
+                flags: JSON_THROW_ON_ERROR,
+            );
+        } catch (\JsonException $e) {
+            throw new ApiException(
+                message: 'Invalid JSON response from API while creating resource at ' . $dto::CREATE_PATH,
+                code: $e->getCode(),
+                previous: $e,
+            );
+        }
 
         $data = $responseData['value'] ?? $responseData;
 
