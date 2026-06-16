284c8f8aa43af0a42d84b7be3358b5eaef459c30
jbevanac <62661464+jbevanac@users.noreply.github.com>
2026-06-16 12:20:10 +0200

    Switch examples config from config.php to .env-based setup

--- examples/00-setup.php
+++ examples/00-setup.php
@@ -2,8 +2,16 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-if (file_exists(__DIR__ . '/../config.php')) {
-    require __DIR__ . '/../config.php';
-} else {
-    require __DIR__ . '/../config.sample.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->safeLoad();
+
+if (file_exists(__DIR__ . '/../.env.local')) {
+    $dotenv = Dotenv\Dotenv::createMutable(__DIR__ . '/..', '.env.local');
+    $dotenv->load();
 }
+
+define('URL', $_ENV['TRIPLETEX_URL']);
+define('CONSUMER_TOKEN', $_ENV['TRIPLETEX_CONSUMER_TOKEN']);
+define('EMPLOYEE_TOKEN', $_ENV['TRIPLETEX_EMPLOYEE_TOKEN']);
+define('CACHE_DIR', dirname(__DIR__) . '/' . ltrim($_ENV['TRIPLETEX_CACHE_DIR'] ?? 'var/cache', '/'));
+define('WEBHOOK_TARGET_URL', $_ENV['TRIPLETEX_WEBHOOK_TARGET_URL'] ?? '');
